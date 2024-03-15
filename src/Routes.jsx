6dd7d48f97a01019069740002d4f7dc29c2a3bbe
8vien8 <vudinhvien890@gmail.com>
2024-03-15 11:31:40 +0700

    Complete login form

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -5,13 +5,16 @@ import Home from './pages/Home';
 import About from './pages/About';
 import NotFound from './pages/NotFound';
 import Contact from './pages/Contact';
+import Login from './components/loginForm/LoginForm';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         {/* <Route path="/" element={<App />} > */}
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home />} >
+              <Route path="/login" element={<Login />} />
+            </Route>  
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
