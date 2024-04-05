d4d40307162873e24465847bf1839adc32d2943a
8vien8 <vudinhvien890@gmail.com>
2024-04-05 09:24:27 +0700

    re-routing depend on login role

---
+++ mockdata.js
@@ -0,0 +1,8 @@
+const mockUsers = [
+    { id: 1, email: 'admin@example.com', password: 'admin123', role: 'admin' },
+    { id: 2, email: 'manager@example.com', password: 'manager123', role: 'manager' },
+    { id: 3, email: 'coordinator@example.com', password: 'coordinator123', role: 'coordinator' },
+    { id: 4, email: 'student@example.com', password: 'student123', role: 'student' },
+];
+
+export default mockUsers;
